addb3af79b40142b31b8cb637849ed98bf526f68
Avocodev <avocode@dev.com>
2025-04-18 16:06:23 +0700

    Make design calculator page using modules

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let i = 0;
-const paragraphElement = document.querySelector(".tooltip-message > p");
-let txt = "Selamat datang di Calculatorku";
-
-function typeEffect() {
-  if (i < txt.length) {
-    paragraphElement.innerHTML += txt.charAt(i);
-    i++;
-
-    setTimeout(() => {
-      typeEffect();
-    }, 50);
-  }
-}
+import typeEffect from "./script/typeEffect.js";
 
 typeEffect();
+
+const startButton = document.querySelector(".js-start-button");
+startButton.addEventListener("click", () => {
+  document.querySelector(".calculator-main").style.display = "block";
+
+  document.querySelector(".welcome-page-main").style.display = "none";
+});
+
+const homeButton = document.querySelector(".js-return-to-home-link");
+homeButton.addEventListener("click", () => {
+  document.querySelector(".calculator-main").style.display = "none";
+
+  document.querySelector(".welcome-page-main").style.display = "block";
+});

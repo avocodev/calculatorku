06dd138346f231a756065d8e12f4198d96eadf34
Avocodev <avocode@dev.com>
2025-04-18 14:04:03 +0700

    Make a typeEffect animation on paragraph

---
+++ script.js
@@ -0,0 +1,16 @@
+let i = 0;
+const paragraphElement = document.querySelector(".tooltip-message > p");
+let txt = "Selamat datang di Calculatorku";
+
+function typeEffect() {
+  if (i < txt.length) {
+    paragraphElement.innerHTML += txt.charAt(i);
+    i++;
+
+    setTimeout(() => {
+      typeEffect();
+    }, 50);
+  }
+}
+
+typeEffect();
